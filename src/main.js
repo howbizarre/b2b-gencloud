154ea7bc48ab8d2330da1d4613d8401c3c853936
How Bizarre <howbizarre@gmail.com>
2023-12-29 13:10:08 +0200

    Update Vue devtools configuration

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ import App from "./App.vue";
 
 __VUE_OPTIONS_API__ = true;
 __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
-
-if (process.env.NODE_ENV !== 'production') {
-  __VUE_PROD_DEVTOOLS__ = true;
-} else {
-  __VUE_PROD_DEVTOOLS__ = false;
-}
+__VUE_PROD_DEVTOOLS__ = false;
 
 const app = createApp(App);
 app.mount("#app");
