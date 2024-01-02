e9d6fb0b3118cfb6751c8a80da34d379b1201945
How Bizarre <howbizarre@gmail.com>
2024-01-02 11:44:26 +0200

    Update Nuxt configuration and package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  'routeRules': {
+  "app": {
+    baseURL: "/b2b",
+  },
+
+  "routeRules": {
     "/": {
-      'prerender': true,
+      "prerender": true,
+      "ssr": false,
+    },
+    "/b2b": {
+      "prerender": true,
+      "ssr": false,
     },
   },
 
-  'css': ["~/assets/css/main.css"],
+  "css": ["~/assets/css/main.css"],
 
-  'postcss': {
-    'plugins': {
-      'tailwindcss': {},
-      'autoprefixer': {},
+  "postcss": {
+    "plugins": {
+      "tailwindcss": {},
+      "autoprefixer": {},
     },
   },
 
-  'devtools': { 'enabled': false },
+  "devtools": { "enabled": false },
 });
